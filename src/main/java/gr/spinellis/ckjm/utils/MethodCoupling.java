b593ced356f982ee93e227159315ccf7e9fc04a5
Marian Jureczko <marian.jureczko@gmail.com>
2025-12-06 18:15:30 +0100

    #69 fix
    - reimplemented compareTo
    - added equals and hashCode

--- src/main/java/gr/spinellis/ckjm/utils/MethodCoupling.java
+++ src/main/java/gr/spinellis/ckjm/utils/MethodCoupling.java
@@ -6,57 +6,56 @@
 package gr.spinellis.ckjm.utils;
 
 
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
- *
  * @author mjureczk
  */
-public class MethodCoupling implements Comparable<MethodCoupling>{
+public class MethodCoupling implements Comparable<MethodCoupling> {
     private String mClassA;
     private String mClassB;
     private String mMethodA;
     private String mMethodB;
-    
 
-    public MethodCoupling( String classA, String methodA, String classB, String methodB) {
+
+    public MethodCoupling(String classA, String methodA, String classB, String methodB) {
         mClassA = classA;
         mMethodA = methodA;
         mClassB = classB;
         mMethodB = methodB;
 
-        if( classA.equals(classB) && methodA.equals(methodB) ){
-            LoggerHelper.printError( "Method "+classA+"."+methodA+" is coupled to itself!", new RuntimeException() );
+        if (classA.equals(classB) && methodA.equals(methodB)) {
+            LoggerHelper.printError("Method " + classA + "." + methodA + " is coupled to itself!", new RuntimeException());
 
-        }
-        else if( classA.equals(classB) ){
-            LoggerHelper.printError( "Coupling within methods in the same class ("+classA+"): "+methodA+" is coupled to "+methodB+"!", new RuntimeException() );
+        } else if (classA.equals(classB)) {
+            LoggerHelper.printError("Coupling within methods in the same class (" + classA + "): " + methodA + " is coupled to " + methodB + "!", new RuntimeException());
         }
 
     }
 
 
     @Override
-    public String toString(){
-        return mClassA+"."+mMethodA+" is coupled to "+mClassB+"."+mMethodB;
+    public String toString() {
+        return mClassA + "." + mMethodA + " is coupled to " + mClassB + "." + mMethodB;
     }
 
     public int compareTo(MethodCoupling mc) {
-        int res=0;
-
-        res = mClassA.compareTo( mc.getClassB() );
-        res += mClassB.compareTo( mc.getClassA() );
-        res += mMethodA.compareTo( mc.getMethodB() );
-        res += mMethodB.compareTo( mc.getMethodA() );
-
-        if( res == 0 ){
-            return res;
+        MethodCoupling reverted = new MethodCoupling(mc.getClassB(), mc.getMethodB(), mc.getClassA(), mc.getMethodA());
+        int compare = exactCompare(reverted);
+        if (compare == 0) {
+            return compare;
         }
+        return exactCompare(mc);
+    }
 
-        res = mClassA.compareTo( mc.getClassA() );
-        res += mClassB.compareTo( mc.getClassB() );
-        res += mMethodA.compareTo( mc.getMethodA() );
-        res += mMethodB.compareTo( mc.getMethodB() );
-
-        return res;
+    private int exactCompare(MethodCoupling mc) {
+        return Comparator
+                .comparing(MethodCoupling::getClassA)
+                .thenComparing(MethodCoupling::getClassB)
+                .thenComparing(MethodCoupling::getMethodA)
+                .thenComparing(MethodCoupling::getMethodB)
+                .compare(this, mc);
     }
 
     /**
@@ -87,6 +86,33 @@ public class MethodCoupling implements Comparable<MethodCoupling>{
         return mMethodB;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof MethodCoupling)) return false;
 
+        MethodCoupling other = (MethodCoupling) obj;
 
+        // Check both orders
+        boolean direct = mClassA.equals(other.mClassA) &&
+                mMethodA.equals(other.mMethodA) &&
+                mClassB.equals(other.mClassB) &&
+                mMethodB.equals(other.mMethodB);
+
+        boolean reversed = mClassA.equals(other.mClassB) &&
+                mMethodA.equals(other.mMethodB) &&
+                mClassB.equals(other.mClassA) &&
+                mMethodB.equals(other.mMethodA);
+
+        return direct || reversed;
+    }
+
+    @Override
+    public int hashCode() {
+        // Since equals is symmetric, hash must also be symmetric
+        // One way: combine the pairs in sorted order
+        int hash1 = Objects.hash(mClassA, mMethodA) + Objects.hash(mClassB, mMethodB);
+        int hash2 = Objects.hash(mClassB, mMethodB) + Objects.hash(mClassA, mMethodA);
+        return hash1 ^ hash2; // XOR ensures symmetry
+    }
 }
--- src/test/java/gr/spinellis/ckjm/utils/MethodCouplingTest.java
+++ src/test/java/gr/spinellis/ckjm/utils/MethodCouplingTest.java
@@ -2,7 +2,12 @@ package gr.spinellis.ckjm.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
@@ -19,9 +24,6 @@ public class MethodCouplingTest {
         assertEquals("ClassA.MethodA is coupled to ClassB.MethodB", mc.toString(), "ToString");
     }
 
-    /**
-     * Test of compareTo method, of class MethodCoupling.
-     */
     @Test
     public void testCompareTo() {
         MethodCoupling mc1 = new MethodCoupling( "ClassA", "MethodA", "ClassB", "MethodB");
@@ -34,12 +36,31 @@ public class MethodCouplingTest {
         assertEquals( 2, mc2.compareTo(mc1), "Compare - not equal");
         assertEquals( -2, mc1.compareTo(mc3), "Compare - not equal" );
         assertEquals( 2, mc3.compareTo(mc1),"Compare - not equal");
-        assertEquals( -2, mc1.compareTo(mc4),"Compare - not equal" );
-        assertEquals( 2, mc4.compareTo(mc1),"Compare - not equal");
+        assertEquals( -1, mc1.compareTo(mc4),"Compare - not equal" );
+        assertEquals( 1, mc4.compareTo(mc1),"Compare - not equal");
         assertEquals( 0, mc2.compareTo(mc3), "Compare - equal" );
         assertEquals( 0, mc3.compareTo(mc2), "Compare - equal" );
         assertEquals( 0, mc2.compareTo(mc4), "Compare - equal" );
         assertEquals( 0, mc4.compareTo(mc2), "Compare - equal" );
     }
 
+    @Test
+    void shouldPassTheCaseFromBugReport69() {
+        MethodCoupling mc1 = new MethodCoupling("Child", "run", "tICAndCBM", "step1");
+        MethodCoupling mc2 = new MethodCoupling("Child", "runReverse", "tICAndCBM", "step2");
+
+        assertEquals(-7, mc1.compareTo(mc2));
+        assertEquals(7, mc2.compareTo(mc1));
+    }
+
+    @Test
+    public void testCompareToTransitivity() {
+        MethodCoupling a = new MethodCoupling("A", "a", "B", "b");
+        MethodCoupling b = new MethodCoupling("B", "b", "A", "c");
+        MethodCoupling c = new MethodCoupling("B", "b", "A", "d");
+
+        assertTrue(a.compareTo(b) < 0, "a < b");
+        assertTrue(b.compareTo(c) < 0, "b < c");
+        assertTrue(a.compareTo(c) < 0, "a < c");
+    }
 }
